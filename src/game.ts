58656e2546b2f9073f960eb9f199a088763da5d6
Karen Marlen <72042911+karen-marlen@users.noreply.github.com>
2021-01-20 14:02:57 -0300

    game config

---
+++ src/game.ts
@@ -0,0 +1,30 @@
+export interface Frame {
+    gameOver: boolean;
+    gameSarted: boolean;
+    width: number;
+    height: number;
+    score: number;
+}
+
+export class GameController {
+    private frame: Frame;
+
+    private velocity = 0;
+
+    constructor(public readonly height = 800, public readonly width = 400) {
+
+    }
+
+    public newGame() {
+        this.frame = {
+            score: 0,
+            width: this.width,
+            height: this.height,
+            gameOver: false,
+            gameSarted: false,
+        };
+
+        return this.frame;
+    }
+
+}
