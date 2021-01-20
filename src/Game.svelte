80ee76e79e3e368476db1e19f85d54b8bb842aa3
Karen Marlen <72042911+karen-marlen@users.noreply.github.com>
2021-01-20 14:08:19 -0300

    game svelte config

---
+++ src/Game.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import { GameController } from "./game";
+    const game = new GameController();
+    let frame = game.newGame();
+</script>
+
+<style>
+    main {
+        position: relative;
+        border: solid black 1px;
+        overflow: hidden;
+        background-color: lightblue;
+    }
+</style>
+
+<main
+    style="width: {frame.width}px; height: {frame.height}px;"
+    class="game"
+>
+</main>
